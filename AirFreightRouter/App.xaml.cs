f1d4444ffc2b9ba59ef1dbc3385865ad519f202a
Chris Woody Woodruff <cwoodruff@live.com>
2026-02-18 18:53:26 -0500

    Solution Setup and Project Scaffolding

---
+++ AirFreightRouter/App.xaml.cs
@@ -0,0 +1,7 @@
+using System.Windows;
+
+namespace AirFreightRouter;
+
+public partial class App : Application
+{
+}
---
+++ AirFreightRouter/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows;
+
+namespace AirFreightRouter;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
